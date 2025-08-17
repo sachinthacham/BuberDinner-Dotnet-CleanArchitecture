a60bc6d0f150ec021bc8c37cbd3105ba75334d7d
sachinthacham <sachinthachamindu26@gmail.com>
2025-08-17 14:27:39 +0530

    update

--- BuberDinner/BuberDinner.Api/Program.cs
+++ BuberDinner/BuberDinner.Api/Program.cs
@@ -1,10 +1,24 @@
+using BuberDinner.Application;
+using BuberDinner.Infrastructure;
+using BuberDinner.Api.Filters;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using BuberDinner.Api.Middleware;
+using BuberDinner.Api.Errors;
+
 var builder = WebApplication.CreateBuilder(args);
 {
+    builder.Services
+    .AddApplication()
+    .AddInfrastructure(builder.Configuration);
+    // builder.Services.AddControllers(options => options.Filters.Add<ErrorHandelingFilterAttribute>());
     builder.Services.AddControllers();
+    builder.Services.AddSingleton<ProblemDetailsFactory,BuberDinnerProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
+    //app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
