26381e8bcaca3add824b5d6c521b9fbb22b688be
sachinthacham <sachinthachamindu26@gmail.com>
2025-08-17 13:11:38 +0530

    implement authentication controller

---
+++ BuberDinner/BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,68 @@
+using BuberDinner.Application.Common.Errors;
+using BuberDinner.Application.Services.Authentication;
+using BuberDinner.Contracts.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using OneOf;
+
+namespace BuberDinner.Api.Controllers;
+
+[ApiController]
+[Route("auth")]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IAuthenticationService _authenticationService;
+
+    public AuthenticationController(IAuthenticationService authenticationService)
+    {
+        _authenticationService = authenticationService;
+    }
+    [HttpPost("register")]
+    public IActionResult Register(RegisterRequest request)
+    {
+       OneOf<AuthenticationResult, DuplicateEmailError> registerResult = _authenticationService.Register(
+            request.FirstName,
+            request.LastName,
+            request.Email,
+            request.Password
+        );
+
+        return registerResult.Match(
+            authResult => Ok(MapAuthResult(authResult)),
+            _ => Problem(statusCode: StatusCodes.Status409Conflict, title: "Email already exists.")
+           
+        );
+
+       
+    }
+
+    private static AuthenticationResponse MapAuthResult(AuthenticationResult authResult)
+    {
+        return new AuthenticationResponse(
+                    authResult.User.Id,
+                    authResult.User.FirstName,
+                    authResult.User.LastName,
+                    authResult.User.Email,
+                    authResult.Token
+
+
+                );
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login(LoginRequest request)
+    {
+         var authenticationResult = _authenticationService.Login(
+           
+            request.Email,
+            request.Password
+        );
+        var response = new AuthenticationResponse(
+            authenticationResult.User.Id,
+            authenticationResult.User.FirstName,
+            authenticationResult.User.LastName,
+            authenticationResult.User.Email,
+            authenticationResult.Token
+        );
+       return Ok(response);
+    }
+}
