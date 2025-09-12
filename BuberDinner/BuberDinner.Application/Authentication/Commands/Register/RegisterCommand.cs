f13405088b844c2852f01c27dbb7dae83463d1b9
sachinthacham <sachinthachamindu26@gmail.com>
2025-09-12 21:48:19 +0530

    implement register command

---
+++ BuberDinner/BuberDinner.Application/Authentication/Commands/Register/RegisterCommand.cs
@@ -0,0 +1,12 @@
+using BuberDinner.Application.Services.Authentication;
+using ErrorOr;
+using MediatR;
+
+namespace BuberDinner.Application.Authentication.Commands.Register;
+
+public record RegisterCommand(
+    string FirstName,
+    string LastName,
+    string Email,
+    string Password
+) : IRequest<ErrorOr<AuthenticationResult>>;
